b201f249a043e69d75ddeb192d34fa94b3db8b73
hyun99999 <ypsophila97@icloud.com>
2020-11-16 00:45:43 +0900

    ajax,ejs

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title1: '이게 타이틀이다~이말이야', content1:'이게 컨텐츠다~이말이야'});
 });
+
+
+
 
 module.exports = router;
